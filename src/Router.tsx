8af8fe591ac0d4447280eaea022af6bec948066a
Pooja R <pooja.ramesh@imaginelearning.com>
2023-04-03 11:08:43 +0530

    Dashboard Basic Design and Cards

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,22 +5,19 @@ import Dashboard from "./screens/Dashboard";
 import Orders from "./screens/Orders";
 import Products from "./screens/Products";
 import Customers from "./screens/Customers";
-import Header from "./components/Header";
 
 const Router = () => {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/orders" element={<Orders />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/customers" element={<Customers />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/customers" element={<Customers />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
