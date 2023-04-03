5ff2e0360fc928390c8bcdb2eb07608e822ef00c
Pooja R <pooja.ramesh@imaginelearning.com>
2023-04-03 11:22:27 +0530

    Dashboard Basic Design and Cards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 
-const Card = () => {
+const Card = (props: {
+  key: number;
+  cardTitle: string;
+  cardDescription: string;
+}) => {
   return (
     <div>
       <div className="col">
         <div className="card">
           <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">
-              This is a longer card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </p>
+            <h5 className="card-title">{props.cardTitle}</h5>
+            <p className="card-text">{props.cardDescription}</p>
           </div>
         </div>
       </div>
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -4,6 +4,33 @@ import SideBar from "../components/SideBar";
 import Card from "../components/Card";
 
 const Home = () => {
+  const cardContents = [
+    {
+      id: 1,
+      title: "Card title 1",
+      desc: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+    },
+    {
+      id: 2,
+      title: "Card title 2",
+      desc: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+    },
+    {
+      id: 3,
+      title: "Card title 3",
+      desc: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+    },
+    {
+      id: 4,
+      title: "Card title 4",
+      desc: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+    },
+    {
+      id: 5,
+      title: "Card title 5",
+      desc: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+    },
+  ];
   return (
     <div>
       <Header />
@@ -11,16 +38,13 @@ const Home = () => {
       <div className="home position-absolute">
         <h1>Welcome Home!</h1>
         <div className="row row-cols-1 row-cols-md-3 g-4 justify-content-evenly mt-3 w-100">
-          <Card />
-          <Card />
-        </div>
-        <div className="row row-cols-1 row-cols-md-3 g-4 justify-content-evenly mt-3 w-100">
-          <Card />
-          <Card />
-        </div>
-        <div className="row row-cols-1 row-cols-md-3 g-4 justify-content-evenly mt-3 w-100">
-          <Card />
-          <Card />
+          {cardContents.map((item, index) => (
+            <Card
+              key={index}
+              cardTitle={item.title}
+              cardDescription={item.desc}
+            />
+          ))}
         </div>
       </div>
       <Footer />
