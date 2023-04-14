9008637f24f08dfbd97da3b089143c34591d65de
Pooja R <pooja.ramesh@imaginelearning.com>
2023-04-14 11:32:36 +0530

    Initial Commit

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,6 +1,22 @@
+import { useState } from "react";
+
 const Table = (props: any) => {
+  const [searchedVal, setSearchedVal] = useState("");
+
   return (
     <>
+      <form className="d-flex mt-3 mb-3 float-end w-50">
+        <input
+          className="form-control me-2"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+          onChange={(e) => setSearchedVal(e.target.value)}
+        />
+        <button className="btn btn-outline-success" type="button">
+          Search
+        </button>
+      </form>
       <table className="table table-dark table-striped border-info table-hover">
         <thead>
           <tr>
@@ -12,17 +28,26 @@ const Table = (props: any) => {
           </tr>
         </thead>
         <tbody>
-          {props.apiData.map((item: any, index: number) => {
-            return (
-              <tr key={index}>
-                <td>{item.id}</td>
-                <td>{item.name}</td>
-                <td>{item.username}</td>
-                <td>{item.email}</td>
-                <td>{item.phone}</td>
-              </tr>
-            );
-          })}
+          {props.apiData
+            .filter(
+              (row: any) =>
+                !searchedVal.length ||
+                row.name
+                  .toString()
+                  .toLowerCase()
+                  .includes(searchedVal.toString().toLowerCase())
+            )
+            .map((item: any, index: number) => {
+              return (
+                <tr key={index}>
+                  <td>{item.id}</td>
+                  <td>{item.name}</td>
+                  <td>{item.username}</td>
+                  <td>{item.email}</td>
+                  <td>{item.phone}</td>
+                </tr>
+              );
+            })}
         </tbody>
       </table>
     </>
